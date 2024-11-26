682bb2a5933f9a5f809f2df9bf578075b6b3295d
Manminder <ManminderSingh@edu.sait.ca>
2024-11-25 18:26:35 -0700

    seee

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import DogLayout from "./Dogsinfo/doglayout";
 
 export default function Page() {
   return (
-    <>
-      <br />
-      <DogLayout />
-    </>
+    <div className="p-4 bg-gray-100 min-h-screen">
+      <div className="container mx-auto">
+        <br className="my-4" />
+        <DogLayout />
+      </div>
+    </div>
   );
 }

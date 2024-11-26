62cbd762803709d8530cf8cad7fd7fdca5cea49f
Manminder <ManminderSingh@edu.sait.ca>
2024-11-25 18:24:22 -0700

    some changes to revert

--- app/page.js
+++ app/page.js
@@ -1,18 +1,8 @@
 import DogLayout from "./Dogsinfo/doglayout";
 
 export default function Page() {
-  const handleClick = () => {
-    alert("🐶");
-  };
-
   return (
     <>
-      <div
-        className="text-2xl font-bold text-center mt-4 hover:text-blue-500"
-        onClick={handleClick}
-      >
-        Dogs-Data
-      </div>
       <br />
       <DogLayout />
     </>

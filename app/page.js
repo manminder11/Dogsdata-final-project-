93f2119e68e0dd16930b0801f209af597a12a8ae
Manminder <ManminderSingh@edu.sait.ca>
2024-11-25 18:28:08 -0700

    cc

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import DogLayout from "./Dogsinfo/doglayout";
 
 export default function Page() {
   return (
-    <div className="p-4 bg-gray-100 min-h-screen flex items-center justify-center">
+    <div className="p-4 bg-gray-500 min-h-screen flex items-center justify-center">
       <div className="container mx-auto bg-white shadow-lg rounded-lg p-6">
         <h1 className="text-2xl font-bold mb-4 text-center">Dog Information</h1>
         <DogLayout />

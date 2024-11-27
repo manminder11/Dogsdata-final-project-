0e7007c5958e3f6618c601d9200a03251d8fe89e
Manminder <ManminderSingh@edu.sait.ca>
2024-11-27 14:12:52 -0700

    sww

--- app/page.js
+++ app/page.js
@@ -1,15 +1,14 @@
 "use client";
 
-
 import React from 'react';
 import DogLayout from "./Dogsinfo/doglayout";
 
 export default function Page() {
   return (
-    <div className="p-4 min-h-screen flex items-center justify-center bg-gray-100 text-black">
-      <div className="container mx-auto bg-white shadow-lg rounded-lg p-6">
-        <div className="d-flex justify-content-between align-items-center mb-4">
-          <h1 className="text-2xl font-bold text-center">Dog Information</h1>
+    <div className="p-8 min-h-screen flex items-center justify-center bg-gray-100 text-black">
+      <div className="container mx-auto bg-white shadow-lg rounded-lg p-8">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-3xl font-bold text-center">Dog Information</h1>
         </div>
         <DogLayout />
       </div>
